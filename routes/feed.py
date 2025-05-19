3a7734a257d0c0fd5a2ef4deeea7de750f26e9fe
Ben Davis <bendavis493@gmail.com>
2025-05-19 16:43:57 -0600

    simplified feed endpoint outputs

--- routes/feed.py
+++ routes/feed.py
@@ -17,12 +17,12 @@ async def get_feed(request: Request):
     user_recipes = FeedController.get_user_recipes(request.state.household_id)
     all_recipes = FeedController.get_suggested_recipes()
     sorted_recipes = FeedController.sort_recipes(request.state.household_id, [*user_recipes, *all_recipes])
-    return {"feed": sorted_recipes}
+    return sorted_recipes
 
 @router.get('/search/{query}')
 async def search_feed(query: str, request: Request):
     user_recipes = FeedController.get_user_recipes(request.state.household_id, keyword=query)
     all_recipes = FeedController.search_all_recipes(query)
     sorted_recipes = FeedController.sort_recipes(request.state.household_id, [*user_recipes,*all_recipes])
-    # Implement search logic here
-    return {'sorted_recipes': sorted_recipes}
+    
+    return sorted_recipes
